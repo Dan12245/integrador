1535dde1505cd252e6eb096b48f94cd5a22f9514
Daniel Isaias De Los Santos <danielisaiasdelossantos@gmail.com>
2025-10-10 09:11:57 -0600

    Front Visual Studio
    Cosas del front jaja

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace C.R.A_Consumo_reducido_de_agua
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void CambiarEscena(UserControl nuevoControl)
+        {
+            Login_Window.Children.Clear();
+            Login_Window.Children.Add(nuevoControl);
+        }
+
+        private void Boton_Register_Click(object sender, RoutedEventArgs e)
+        {
+            CambiarEscena(new Login());
+        }
+    }
+}
