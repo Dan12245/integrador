a814a75ae2c6a14706ef5e43d5ee52411e6ccbf8
AlexanderMoon04 <alexitrix@gmail.com>
2025-10-30 07:23:10 -0600

    Implementation of a new menu in progress....

---
+++ Controls/Menu.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Consumo_Reducido_de_Agua_ahora_si_definitivo.Controls
+{
+    /// <summary>
+    /// Logique d'interaction pour Menu.xaml
+    /// </summary>
+    public partial class Menu : UserControl
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Boton_Menu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Boton_Configuracion_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Boton_Usuario_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Boton_Reportar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Inicio_Help_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
